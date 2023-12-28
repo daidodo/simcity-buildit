26e5b74a5c337c1da5e5b20c4ee9336ca32b8899
Zhao DAI <zddai@amazon.com>
2023-12-28 23:56:49 +0000

    Imp tests.

--- src/game/data.test.ts
+++ src/game/data.test.ts
@@ -16,7 +16,7 @@ function testProduction(production: ProductionData) {
   const { requirements } = production;
   if (requirements && requirements.length > 0) {
     describe('Requirements', () => {
-      production.requirements?.forEach(name => {
+      [...new Set(requirements)].forEach(name => {
         describe(name, () => {
           it('should be defined', () => {
             expect(ALL_PRODUCTIONS.some(p => p.name === name)).toBeTruthy();

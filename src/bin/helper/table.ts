daadcf85383101672745b366e98f44e095c7acb6
Zhao DAI <zddai@amazon.com>
2023-12-30 22:03:11 +0000

    supprt multiple titles.

--- src/bin/helper/table.ts
+++ src/bin/helper/table.ts
@@ -7,24 +7,25 @@ const DEFAULT_OPTIONS = {
 
 type Options = Partial<typeof DEFAULT_OPTIONS>;
 
-export function formatTable(title: string[], content: string[][], options?: Options) {
+export function formatTable(title: string[][], content: string[][], options?: Options) {
   // options
   const cs = options?.minimalColunmSize ?? DEFAULT_OPTIONS.minimalColunmSize;
   const sc = options?.spacesBetweenColumns ?? DEFAULT_OPTIONS.spacesBetweenColumns;
   // calc column lengths
-  const cl = [title, ...content].reduce((r, a) => {
+  assertTrue(title.length > 0);
+  const cl = [...title, ...content].reduce((r, a) => {
     assertTrue(a.length <= r.length);
     for (let i = 0; i < r.length; ++i) {
       if (a[i] === undefined) a[i] = '';
       r[i] = Math.max(r[i], a[i].length);
     }
     return r;
-  }, Array(title.length).fill(cs));
-  const lines = [title, ['---'], ...content].map(row =>
+  }, Array(title[0].length).fill(cs));
+  const lines = [...title, ['---'], ...content].map(row =>
     row
       .map((c, i) => c.padEnd(cl[i], ' '))
       .join(' '.repeat(sc))
-      .trim(),
+      .trimEnd(),
   );
   return lines.join('\n');
 }
